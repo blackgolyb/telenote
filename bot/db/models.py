f355ed50398a223ae30a30d23aa4a7b33bb4f9ee
Blackgolyb <andrejomelnickij@gmail.com>
2023-10-26 08:18:46 +0300

    update user model and user DAL

--- bot/db/models.py
+++ bot/db/models.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String
+from sqlalchemy import Column, BigInteger, String, Boolean
 
 from bot.db.base import Base
 
@@ -8,6 +8,8 @@ class User(Base):
 
     user_id = Column(BigInteger, primary_key=True, unique=True, autoincrement=False)
     github_token = Column(String(100))
-    note_path = Column(String(300))
+    note_file = Column(String(300))
     notes_repository = Column(String(50))
     notes_branch = Column(String(50))
+    assets_folder = Column(String(300))
+    is_registered = Column(Boolean(False))
--- bot/services/user_dal.py
+++ bot/services/user_dal.py
@@ -1,4 +1,4 @@
-from sqlalchemy import select
+from sqlalchemy import select, update
 
 from bot.db.models import User
 
@@ -9,24 +9,13 @@ class BaseDAL(object):
 
 
 class UserDAL(BaseDAL):
-    async def create_user(
-        self,
-        user_id: int,
-        github_token: str,
-        note_path: str,
-        notes_repository: str,
-        notes_branch: str,
-    ):
-        await self.session.merge(
-            User(
-                user_id=user_id,
-                github_token=github_token,
-                note_path=note_path,
-                notes_repository=notes_repository,
-                notes_branch=notes_branch,
-            )
+    async def create_user(self, user_id: int, **kwargs) -> None:
+        new_user = User(
+            user_id=user_id,
+            **kwargs,
         )
-        await self.session.commit()
+        self.session.add(new_user)
+        await self.session.flush()
 
     async def get_user_by_id(self, user_id: int) -> User:
         query = select(User).where(User.user_id == user_id)
@@ -34,3 +23,12 @@ class UserDAL(BaseDAL):
         user_row = res.fetchone()
         if user_row is not None:
             return user_row[0]
+
+    async def update_user(self, user_id: int, **kwargs) -> None:
+        query = (
+            update(User)
+            .where(User.user_id == user_id)
+            .values(kwargs)
+            .returning(User.user_id)
+        )
+        await self.db_session.execute(query)
