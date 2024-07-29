7f22f43859043feeb52e65276e66deea0958216b
Blackgolyb <andrejomelnickij@gmail.com>
2024-07-29 22:55:07 +0300

    replace dataclasses to pydantic-settings

--- bot/config/__init__.py
+++ bot/config/__init__.py
@@ -1,4 +1,11 @@
-from .config_loader import load_config, Config
-
+from .config_loader import Config, load_config
 
 config: Config = load_config()
+
+def get_secret_key() -> str:
+    return config.core.secret_key
+
+def get_db_url() -> str:
+    return str(config.db.url)
+
+__all__ = ["config", "get_secret_key", "get_db_url"]
--- bot/config/config_loader.py
+++ bot/config/config_loader.py
@@ -1,63 +1,87 @@
-from dataclasses import dataclass
+from pathlib import Path
+from typing import Optional
+
 from envparse import Env
+from pydantic import (
+    BaseModel,
+    Field,
+    PostgresDsn,
+    ValidationInfo,
+    field_validator,
+)
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+PROJECT_FOLDER = Path(__file__).parent.parent.parent
 
 
-@dataclass
-class Bot:
-    token: str
+def get_model_config(**kwargs):
+    return SettingsConfigDict(
+        env_nested_delimiter="__",
+        case_sensitive=False,
+        **kwargs,
+    )
 
 
-@dataclass
-class General:
-    secret_key: str
-
-
-@dataclass
-class DB:
+class DB(BaseSettings):
+    driver: str
     host: str
     port: int
-    name: str
     user: str
     password: str
+    db: str
 
-    class Config:
-        db_type: str = "postgresql"
-        db_interface: str = "psycopg"
+    url: Optional[PostgresDsn] = Field(None, validate_default=True)
 
-    @property
-    def db_url(self):
-        url_fmt = "{db_type}+{db_interface}://{user}:{password}@{host}:{port}/{db_name}"
+    model_config = get_model_config(env_prefix="POSTGRES_")
 
-        return url_fmt.format(
-            db_type=self.Config.db_type,
-            db_interface=self.Config.db_interface,
-            user=self.user,
-            password=self.password,
-            host=self.host,
-            port=self.port,
-            db_name=self.name,
+    @field_validator("url", mode="before")
+    @classmethod
+    def assemble_db_connection(cls, v: Optional[str], values: ValidationInfo) -> str:
+        if isinstance(v, str):
+            return v
+
+        return PostgresDsn.build(
+            scheme=f"postgresql+{values.data.get('driver')}",
+            username=values.data.get("user"),
+            password=values.data.get("password"),
+            host=f"{values.data.get('host')}:{values.data.get('port')}",
+            path=f"{values.data.get('db') or ''}",
         )
 
 
-@dataclass
-class Config:
+class Bot(BaseModel):
+    token: str
+
+
+class Core(BaseModel):
+    secret_key: str
+
+
+class Config(BaseSettings):
     bot: Bot
-    db: DB
-    general: General
+    db: DB = Field(default_factory=DB)
+    core: Core = Field(default_factory=Core)
+
+    model_config = get_model_config()
+
+
+def load_env_by_env_type(
+    env: Env,
+    env_type: str,
+    certs_folder: Path,
+) -> None:
+    env_file_name = f".env.{env_type.lower()}"
+    env_file = certs_folder / env_file_name
+
+    if env_file.exists():
+        env.read_envfile(env_file)
+    else:
+        raise FileNotFoundError(f"Environment file not found: {env_file}")
 
 
 def load_config() -> Config:
     env = Env()
-    env.read_envfile()
+    env_type = env.str("ENV_TYPE", default="dev")
+    load_env_by_env_type(env, env_type, PROJECT_FOLDER / "certs")
 
-    return Config(
-        bot=Bot(token=env.str("BOT_TOKEN")),
-        db=DB(
-            host=env.str("DB_HOST"),
-            port=env.str("DB_PORT"),
-            name=env.str("DB_NAME"),
-            user=env.str("DB_USER"),
-            password=env.str("DB_PASSWORD"),
-        ),
-        general=General(secret_key=env.str("SECRET_KEY")),
-    )
+    return Config()
